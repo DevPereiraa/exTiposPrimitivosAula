365a399ea0c17ba323b665413d75e10728ec6812
Gustavo Pereira <gustavo.cabral.campos1@gmail.com>
2024-12-03 14:47:59 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String nome = "Gustavo";
+        String endereco = "São Paulo, Brasil";
+        double salario = 9810.22;
+        String dataRecebimento = "05/12/2024";
+        String relatorio = "Eu " + nome + ", morando no endereço: " + endereco + ", confirmo que recebi o salário de: R$" + salario + " na data " + dataRecebimento;
+
+        System.out.println(relatorio);
+    }
+}
